a51a08a475462389f0a9846b613d10494da61cde
evalincius <ed@oa-edu-169-129.wireless.abdn.ac.uk>
2014-12-15 16:53:52 +0000

    query
    example works

---
+++ a.php
@@ -0,0 +1,56 @@
+<html>
+  <body>
+ 
+  <?php
+  /* ARC2 static class inclusion */ 
+  include_once('semsol/ARC2.php');  
+ 
+  $dbpconfig = array(
+  "remote_store_endpoint" => "http://www.linkedmdb.org/sparql",
+   );
+ 
+  $store = ARC2::getRemoteStore($dbpconfig); 
+ 
+  if ($errs = $store->getErrors()) {
+     echo "<h1>getRemoteSotre error<h1>" ;
+  }
+ 
+  $query = '
+    PREFIX mdb: <http://data.linkedmdb.org/resource/movie/film>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dc: <http://purl.org/dc/terms/>
+
+    SELECT ?label?resource WHERE {
+      ?resource mdb:id ?uri .
+      ?resource dc:title ?label . 
+      FILTER regex(?label,"Batman")
+    }';
+  
+  /* execute the query */
+  $rows = $store->query($query, 'rows'); 
+ 
+    if ($errs = $store->getErrors()) {
+       echo "Query errors" ;
+       print_r($errs);
+    }
+ 
+    /* display the results in an HTML table */
+    echo "<table border='4'>
+    <thead>
+        <th>#</th>
+        <th>Species (Label)</th>
+        <th>Species (Label)</th>
+    </thead>";
+
+    /* loop for each returned row */
+    foreach( $rows as $row ) { 
+    print "<tr><td>".++$id. "</td><td>
+    <a href='". $row['species'] . "'>" . 
+    $row['label']."</a></td><td>" . 
+    $row['resource']. "</td></tr>";
+    }
+    echo "</table>" 
+
+  ?>
+  </body>
+</html>
